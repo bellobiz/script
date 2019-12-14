81c17e6b5a2b9bae3433b4d19f88b1f7e7948aae
belloosagienoah <osagienoah555@gmail.com>
2019-12-14 03:44:18 -1100

    basic-1

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-function speak(string) {
-    if (string === string.toLowerCase()) {
-        return 'I love you a lot';
-    }
-    else if (string === string.toUpperCase()) {
-        return 'I think I love you';
-    }
-    else if (string == 'Hello Grandma') {
-        return 'I love you grandma';
-    }
-    else {
-        return 'Invalid sting'
-    }
+var a, b;
+
+a = Math.floor(Math.random() * 1000);
+b = Math.floor(Math.random() * 1000);
+
+function values(a, b) {
+    console.log(`a = ${a}; b = ${b}`);
 }
 
-console.log(speak('hello'))
+function add(a, b) {
+    console.log(`Addition = ${a + b}`);
+}
+
+function subtract(a, b) {
+    console.log(`Subtraction = ${a - b}`);
+}
+
+function multiply(a, b) {
+    console.log(`Multiplication = ${a * b}`);
+}
+
+function divide(a, b) {
+    console.log(`Division = ${a / b}`);
+}
+
+function increase(a) {
+    console.log(`Increment a (by one) = ${a + 1}`);
+}
+
+function decrease(a) {
+    console.log(`Decrement (by one) = ${a - 1}`);
+}
+
+function output() {
+    values(a, b);
+    add(a, b);
+    subtract(a, b);
+    multiply(a, b);
+    divide(a, b);
+    increase(a);
+    decrease(a);
+}
+
+output();
